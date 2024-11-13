f6f03af99f2f3eef16a008cc7a10fc8f0608786d
TheLife <nicholastoya@yahoo.com.br>
2024-11-12 22:51:59 -0300

    UI stuff

--- Assets/Game/Scripts/Gameplay/PlayerController.cs
+++ Assets/Game/Scripts/Gameplay/PlayerController.cs
@@ -1,9 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.UIElements;
 
 namespace Cinetica.Gameplay
 {
     public class PlayerController : MonoBehaviour
     {
+        private UIDocument gui;
         
+        private void Awake()
+        {
+            GameManager.Instance.OnFingerDown.AddListener(OnFingerDown);
+        }
+
+        public void OnFingerDown(Finger finger)
+        {
+            
+        }
+        
+        public void MoveCameraTo(Transform t)
+        {
+            
+        }
     }
 }
